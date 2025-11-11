219b4b92ad164a483b852abcd44c1b2d051527e9
siwookim1114 <swk20001114@gmail.com>
2025-11-11 21:33:36 +0800

    Organized script to download all images + annotations (2014, 2017) + added requirements.txt

--- download_img.py
+++ download_img.py
@@ -6,24 +6,7 @@ from tqdm import tqdm
 # Config
 DATA_DIR = "data/raw"
 
-# COCO
-TRAIN_URL = "http://images.cocodataset.org/zips/train2017.zip"
-VAL_URL = "http://images.cocodataset.org/zips/val2017.zip"
-
-# ZIP FILE
-TRAIN_ZIP = os.path.join(DATA_DIR, "train2017.zip")
-VAL_ZIP = os.path.join(DATA_DIR, "val2017.zip")
-TRAIN_DIR = os.path.join(DATA_DIR, "train")
-VAL_DIR = os.path.join(DATA_DIR, "valid")
-
-# COCO Annotations
-ANNOT_URL = "http://images.cocodataset.org/annotations/annotations_trainval2014.zip"
-ANNOT_ZIP = os.path.join(DATA_DIR, "annotations_trainval2014.zip")
-ANNOT_DIR = os.path.join(DATA_DIR, "annotations/annotations2014")
-
-# More Extra datasets
-
-EXTRA_DATASETS = [
+DATASETS = [
     {
         "name": "COCO 2014 Train",
         "url": "http://images.cocodataset.org/zips/train2014.zip",
@@ -37,16 +20,34 @@ EXTRA_DATASETS = [
         "dir": os.path.join(DATA_DIR, "val2014")
     },
     {
+        "name": "COCO 2017 Train",
+        "url": "http://images.cocodataset.org/zips/train2017.zip",
+        "zip": os.path.join(DATA_DIR, "train2017.zip"),
+        "dir": os.path.join(DATA_DIR, "train2017")
+    },
+    {
+        "name": "COCO 2017 Val",
+        "url": "http://images.cocodataset.org/zips/val2017.zip",
+        "zip": os.path.join(DATA_DIR, "val2017.zip"),
+        "dir": os.path.join(DATA_DIR, "val2017")
+    },
+    {
         "name" : "COCO Test 2017",
         "url": "http://images.cocodataset.org/zips/test2017.zip",
         "zip": os.path.join(DATA_DIR, "test2017.zip"),
         "dir": os.path.join(DATA_DIR, "test2017")
     },
     {
-        "name": "Flickr30k Images",
-        "url": "https://cs.stanford.edu/people/karpathy/deepimagesent/flickr30k-images.zip",
-        "zip": os.path.join(DATA_DIR, "flickr30k.zip"),
-        "dir": os.path.join(DATA_DIR, "flickr30k")
+        "name": "COCO 2014 Annotations",
+        "url": "http://images.cocodataset.org/annotations/annotations_trainval2014.zip",
+        "zip": os.path.join(DATA_DIR, "annotations_trainval2014.zip"),
+        "dir": os.path.join(DATA_DIR, "annotations/annotations2014")
+    },
+    {
+        "name": "COCO 2017 Annotations",
+        "url": "http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
+        "zip": os.path.join(DATA_DIR, "annotations_trainval2017.zip"),
+        "dir": os.path.join(DATA_DIR, "annotations/annotations2017")
     }
 ]
 
@@ -93,21 +94,11 @@ class Downloader():
         print(f"Extraction complete: {self.extract_dir}")
     
 if __name__ == "__main__":
-    # Coco
-    # train_downloader = ImageDownloader(TRAIN_URL, TRAIN_ZIP, TRAIN_DIR)
-    # val_downloader = ImageDownloader(VAL_URL, VAL_ZIP, VAL_DIR)
+    for ds in DATASETS:
+        downloader = Downloader(ds['url'], ds['zip'], ds['dir'])
+        downloader.download()
+        downloader.extract()
 
-    # for ds in EXTRA_DATASETS:
-    #     downloader = ImageDownloader(ds['url'], ds['zip'], ds['dir'])
-    #     downloader.download()
-    #     downloader.extract()
-
-    # COCO 2017 Annotations
-    annot_downloader = Downloader(ANNOT_URL, ANNOT_ZIP, ANNOT_DIR)
-    annot_downloader.download()
-    annot_downloader.extract()
-
-    print(f"Annotations: {ANNOT_DIR}/annotations")
     print("All done!")
     
 
